598583de68fe495d46c6f177db81960dd673253f
Xavier Graterol <xavier.graterol@gmail.com>
2015-04-15 05:47:50 -0430

    [FIX] AVATAR RESIZE PARA DISMINUIR TIEMPO DE CARGA

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,8 +3,8 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  #include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::RMagick
+  #include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -25,7 +25,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  #process :resize_to_fit => [500, 800]
+  process :resize_to_fit => [500, 800]
   #
   # def scale(width, height)
   #   # do something
