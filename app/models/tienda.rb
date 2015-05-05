9e506f1476c0ad4f91d46b53217b047aa54d56d7
Xavier Graterol <xavier.graterol@gmail.com>
2015-05-05 13:31:05 -0430

    [FIX] NIVEL MALL ESTADISTICAS INTERMENSUALES

--- app/models/tienda.rb
+++ app/models/tienda.rb
@@ -111,15 +111,11 @@ class Tienda < ActiveRecord::Base
         total = 0
         total_usd = 0
         nivel_mall.tiendas.joins(:nivel_mall, :actividad_economica, :tipo_local, :contrato_alquilers).by_nivel_mall(nivel_mall_id).by_actividad_economica(actividad_economica_id).by_rango_contrato(fecha_init, fecha_end).by_tipo_local(tipo_local_id).each do |tienda|
-          # ventas += ventas
           ventas = ventas + tienda.ventas.sum(:monto_ml)
           canon_fijo = canon_fijo + tienda.pago_alquilers.sum(:monto_canon_fijo_ml)
           porc_canon = porc_canon + tienda.pago_alquilers.sum(:monto_porc_ventas_ml)
           total = total + tienda.pago_alquilers.sum(:monto_alquiler_ml)
           total_usd = total_usd + tienda.pago_alquilers.sum(:monto_alquiler_usd)
-          # tienda.contrato_alquilers.each do |contrato|
-          #   ventas = ventas_x_contrato(contrato, ventas)
-          # end
         end
         hash_stats = Hash.new
         hash_stats[:canon_fijo_ml] = canon_fijo
@@ -127,6 +123,7 @@ class Tienda < ActiveRecord::Base
         hash_stats[:total] = total
         hash_stats[:total_usd] = total_usd
         hash_stats[:criterio] = nivel_mall.nombre
+        hash_stats[:ventas] = ventas
         estadisticas << hash_stats
       end
     elsif criterio == 'actividad_economica'
